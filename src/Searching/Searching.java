e33ecaf40e3f8e0238f752772796ef759c190c70
Bartłomiej Dettlaff <bartlomiejdettlaff@interia.pl>
2018-06-28 10:30:51 +0200

    Finding lessons in progress

--- src/Searching/Searching.java
+++ src/Searching/Searching.java
@@ -1,67 +1,123 @@
 package Searching;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Searching {
-/*
-    Prospector Searching = new Prospector();
 
-    FileReader fr = null;
-    String linia = "";
+    public void openFile(){
+        FileReader fr = null;
+        String linia = "";
 
-    // OTWIERANIE PLIKU:
-                try {
-        fr = new FileReader("18mar15plany.txt");
-    } catch (FileNotFoundException e) {
-        System.out.println("BŁĄD PRZY OTWIERANIU PLIKU!");
-        System.exit(1);
-    }
-
-    BufferedReader bfr = new BufferedReader(fr);
-    // ODCZYT KOLEJNYCH LINII Z PLIKU:
-                try {
-        while((linia = bfr.readLine()) != null){
-            Searching.searchForLecturer("Duch P",linia);
+        try {
+            fr = new FileReader("18june06.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("BŁĄD PRZY OTWIERANIU PLIKU!");
+            System.exit(1);
         }
-    } catch (IOException e) {
-        System.out.println("BŁĄD ODCZYTU Z PLIKU!");
-        System.exit(2);
-    }
 
-    // ZAMYKANIE PLIKU
-                try {
-        fr.close();
-    } catch (IOException e) {
-        System.out.println("BŁĄD PRZY ZAMYKANIU PLIKU!");
-        System.exit(3);
-    }
-
-
-    public void searchForLecturer(String Sheet, String Line){
-        Pattern surname = Pattern.compile(Sheet);
-        Matcher matcher2 = surname.matcher(Line);
-        if(matcher2.find())
-        {
-            int foundBeginningIndex=0;
-            int foundEndingIndex=0;
-            String group;
-            for(int i=0; i<Line.length(); i++) {
-
-                if(Line.charAt(i) == '>') {
-                    foundBeginningIndex = i+1;
-                }
-
-                if(foundBeginningIndex != 0){
-                    for(i=foundBeginningIndex; i<Line.length(); i++){
-
-                        if(Line.charAt(i) == '<') {
-                            foundEndingIndex = i;
-                            group=Line.substring(foundBeginningIndex,foundEndingIndex);
-                            System.out.println(group);
-                        }
-                    }
-                }
+        BufferedReader bfr = new BufferedReader(fr);
+                    try {
+            while((linia = bfr.readLine()) != null){
+                Searching.searchForLesson("4I01",linia);
             }
+        } catch (IOException e) {
+            System.out.println("BŁĄD ODCZYTU Z PLIKU!");
+            System.exit(2);
+        }
+
+        try {
+            fr.close();
+        } catch (IOException e) {
+            System.out.println("BŁĄD PRZY ZAMYKANIU PLIKU!");
+            System.exit(3);
         }
     }
 
-*/
+    public static void searchForLesson(String Sheet, String Line){
+
+        Pattern patternLine = Pattern.compile(Sheet);
+        Matcher matcherLine = patternLine.matcher(Line);
+        String result="";
+        int indexBeforeTime;
+        char signSemicolon =';';
+        int counterOfSemicolons=0;
+        int i=0;
+        int beginningIndex=0;
+        int endingIndex=0;
+
+        if(matcherLine.find()){
+            String day = Line.substring(1,3);
+            //System.out.println(day);
+            result = Searching.switchDayOfWeek(day).toString()+",";
+
+            indexBeforeTime = Line.indexOf(" ");
+            //System.out.println(Line.substring(indexBeforeTime+1,indexBeforeTime+2));
+
+            result = result + Searching.switchTime(Line.substring(indexBeforeTime+1,indexBeforeTime+3)) + "," + Searching.switchTime(Line.substring(indexBeforeTime+7,indexBeforeTime+9))+ ",";
+            //System.out.println("TEST:" + result);
+        //To trzeba będzie zmienić w pizdu
+
+           while(counterOfSemicolons<4) {
+
+                if(Line.charAt(i) == signSemicolon){
+                    counterOfSemicolons++;
+                }
+
+                if(counterOfSemicolons == 3 && beginningIndex==0){
+                    beginningIndex = i;
+                 //   System.out.println("BEGIN:" + i);
+                }
+                else if(counterOfSemicolons == 4 && endingIndex==0){
+                    endingIndex = i;
+                 //   System.out.println("END:" + i);
+                }
+                i++;
+            }
+
+            result = result + Line.substring(beginningIndex+2,endingIndex-1)+",";
+
+            System.out.println(result);
+        }
+
+
+    }
+
+
+
+    public static Integer switchDayOfWeek(String day){
+
+        if(day.equals("Pn")){
+            return 1;
+        }
+
+        if(day.equals("Wt")){
+            return 2;
+        }
+
+        if(day.equals("Śr")){
+            return 3;
+        }
+
+        if(day.equals("Cz")){
+            return 4;
+        }
+
+        return 5;
+    }
+
+    public static Integer switchTime(String time){
+        try{
+            int timeAfterConversion;
+            timeAfterConversion = Integer.parseInt(time);
+            return timeAfterConversion-7;
+        }catch(NumberFormatException e){
+            return 0;
+        }
+
+    }
 }
--- src/Student/StudentController.java
+++ src/Student/StudentController.java
@@ -1,4 +1,92 @@
 package Student;
 
-public class StudentController {
+import Searching.Searching;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import java.net.URL;
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class StudentController implements Initializable {
+    @FXML
+    private ComboBox<String> listOfGroupsComboBox;
+
+    @FXML
+    private GridPane schedule;
+
+    private Button[][] btn = new Button[6][13];
+
+    private ArrayList<String> daysOfTheWeek = new ArrayList<String>() {{
+        add("Poniedziałek");
+        add("Wtorek");
+        add("Sroda");
+        add("Czwartek");
+        add("Piątek");
+    }};
+
+    private ArrayList<String> hoursInTheDay = new ArrayList<String>() {{
+        add("8.00");
+        add("9.00");
+        add("10.00");
+        add("11.00");
+        add("12.00");
+        add("13.00");
+        add("14.00");
+        add("15.00");
+        add("16.00");
+        add("17.00");
+        add("18.00");
+        add("19.00");
+    }};
+
+    private ObservableList<String> listOfGroups =
+            FXCollections.observableArrayList(
+                    "4I04",
+                    "4I05",
+                    "4I06"
+            );
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        listOfGroupsComboBox.setItems(listOfGroups);
+        insertButtonsTest();
+    }
+
+    public void insertButtonsTest() {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 13; j++) {
+                if (j == 0 && i!=0) {
+                    btn[i][j] = new Button(daysOfTheWeek.get(i-1));
+                   // System.out.println(s.indexOf(" "));
+                    btn[i][j].setPrefSize(165, 60);
+                    schedule.add(btn[i][j], i, j);
+                } else if (i == 0 && j!=0) {
+                    btn[i][j] = new Button(hoursInTheDay.get(j-1));
+                    btn[i][j].setPrefSize(165, 60);
+                    schedule.add(btn[i][j], i, j);
+                } else if( i == 0 && j ==0){
+                    btn[i][j] = new Button();
+                    btn[i][j].setPrefSize(165, 60);
+                    schedule.add(btn[i][j], i, j);
+                } else {
+                    btn[i][j] = new Button("1-15\n Podstawy programowania\n II i24 CTI406\n Cisłak A.");
+                    btn[i][j].setPrefSize(165, 60);
+                    schedule.add(btn[i][j], i, j);
+                }
+
+            }
+        }
+
+        Searching test = new Searching();
+        test.openFile();
+    }
+
+
 }
